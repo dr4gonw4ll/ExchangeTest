ddafd7ecd17f2de77fc11e8eaa8063be464d0167
dr4gonw4ll <anand.namana@gmail.com>
2019-05-03 16:42:23 -0400

    Add files via upload

---
+++ Exchange-2016-CIS-Powershell/mailbox_audit.ps1
@@ -0,0 +1,12 @@
+﻿$servername = hostname
+$filename = $servername + "_mailbox.txt"
+
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+echo "Get-TransportService" >> $filename
+echo "Get-TransportService"
+Get-TransportServer >> $filename
+
+echo "Get-MailboxDatabase" >> $filename
+echo "Get-MailboxDatabase"
+Get-MailboxDatabase | fl -property Identity,IssueWarningQuota,ProhibitSendReceiveQuota,ProhibitSendQuota,MailboxRetention,RetainDeletedItemsUntilBackup,DeletedItemRetention,DeletedItemRetention >> $filename
---
+++ Exchange-2016-CIS-Powershell/um_audit.ps1
@@ -0,0 +1,20 @@
+﻿$servername = hostname
+$filename = $servername + "_um.txt"
+
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+
+echo "Get-UMService" >> $filename
+echo "Get-UMService"
+Get-UMService | fl -property Identity,UMStartUpMode >> $filename
+
+echo "Get-UMDialPlan" >> $filename
+echo "Get-UMDialPlan"
+Get-UMDialPlan | fl -property Identity,VoIPSecurity >> $filename
+
+echo "Get-UMMailboxPolicy" >> $filename
+echo "Get-UMMailboxPolicy"
+Get-UMMailboxPolicy | fl -property Identity,AllowPinlessVoiceMailAccess >> $filename
+
+echo "Get-AdminAuditLogConfig" >> $filename
+echo "Get-AdminAuditLogConfig"
+Get-AdminAuditLogConfig | fl -property Identity,AdminAuditLogCmdlets,AdminAuditLogEnabled >> $filename
